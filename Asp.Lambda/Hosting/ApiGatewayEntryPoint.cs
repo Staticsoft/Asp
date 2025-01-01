b4679057b5adfc928159e4f4c7e927159bf53494
OlegRakovich <devgeslot@gmail.com>
2025-01-01 16:52:03 -0500

    Initial implementation

---
+++ Asp.Lambda/Hosting/ApiGatewayEntryPoint.cs
@@ -0,0 +1,11 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Staticsoft.Asp.Lambda;
+
+class ApiGatewayEntryPoint<Startup> : APIGatewayProxyFunction
+    where Startup : class
+{
+    protected override void Init(IWebHostBuilder builder)
+        => builder.UseStartup<Startup>();
+}
---
+++ Asp.Lambda/Hosting/LambdaEntrypoint.cs
@@ -0,0 +1,44 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.Serialization.SystemTextJson.Converters;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Staticsoft.Asp.Lambda;
+
+public abstract class LambdaEntrypoint<Startup>
+    where Startup : class
+{
+    readonly ApiGatewayEntryPoint<Startup> Entrypoint = new();
+    readonly static JsonSerializerOptions Options = new()
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNameCaseInsensitive = true,
+        PropertyNamingPolicy = new AwsNamingPolicy(),
+        Converters =
+        {
+            new DateTimeConverter(),
+            new MemoryStreamConverter(),
+            new ConstantClassConverter(),
+            new ByteArrayConverter()
+        }
+    };
+
+    protected virtual TriggerCollection Triggers
+        => new([]);
+
+    public Task<APIGatewayProxyResponse> FunctionHandlerAsync(JsonElement request, ILambdaContext lambdaContext)
+    {
+        foreach (var source in Triggers)
+        {
+            if (source.TryConvert(request, Options, out var proxyRequest))
+            {
+                return Entrypoint.FunctionHandlerAsync(proxyRequest, lambdaContext);
+            }
+        }
+
+        var serializedRequest = JsonSerializer.Serialize(request, Options);
+        throw new NotSupportedException($"Unable to handle request: {serializedRequest}");
+    }
+}
